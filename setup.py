5ac0b93d3dc4e873948e5d0d20e0284d0c9f50ae
UlyssesCB <ulysses.informatica@gmail.com>
2020-05-04 15:36:43 -0300

    Change requirements install method

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import sys
-from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
 if sys.version_info < (3, 5):
     raise Exception("Only Python 3.5+ is supported")
-
-raw_requirements = parse_requirements("requirements.txt", session=False)
-requirements = [str(ir.req) for ir in raw_requirements]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -22,7 +18,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/quintoandar/kafka-python",
     packages=find_packages(exclude=["ez_setup", "examples", "tests", "release"]),
-    install_requires=requirements,
+    install_requires=[
+        "redis==2.10.6",
+        "retrying==1.3.3",
+        "kafka-python==2.0.1",
+    ],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
